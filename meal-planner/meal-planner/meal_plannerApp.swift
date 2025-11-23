fc38e1f32b582fcf94ccd95190503a9d498950b2
wigothehacker <wigothehacker@gmail.com>
2025-11-23 19:57:30 +0200

    Runned hello world

---
+++ meal-planner/meal-planner/meal_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  meal_plannerApp.swift
+//  meal-planner
+//
+//  Created by wigothehacker on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct meal_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meal-planner/meal-plannerTests/meal_plannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  meal_plannerTests.swift
+//  meal-plannerTests
+//
+//  Created by wigothehacker on 11/23/25.
+//
+
+import Testing
+@testable import meal_planner
+
+struct meal_plannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
